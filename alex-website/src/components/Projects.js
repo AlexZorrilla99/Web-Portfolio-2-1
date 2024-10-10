77b392dc21880bd05c89b516f44c3a3d78c53ab0
Alex Zorrilla <92313275+AlexZorrilla99@users.noreply.github.com>
2024-10-09 20:42:59 -0400

    new project added

--- alex-website/src/components/Projects.js
+++ alex-website/src/components/Projects.js
@@ -9,15 +9,16 @@ const projects = [
         description: 'This project is a comprehensive strategy platform for the game Deadlock. It allows users to create custom tier lists, plan character placements on an interactive map using drag-and-drop functionality, and includes a secure login system powered by AWS. The entire platform was developed using React, offering a dynamic and user-friendly experience for strategizing and sharing game plans.',
         github: 'https://github.com/shmablo/Deadplant',
         website: 'http://www.deadplant.lol/',
-      },/*
+      },
       
       {
-        title: 'Deadplant (Strategy Making Website)',
-        image: '/assets/images/deadplant2.png', // Public path to the image
-        description: 'This project is a comprehensive strategy platform for the game Deadlock...',
-        github: 'https://github.com/yourproject1',
-        website: 'https://project1.com',
+        title: 'Top Music Genre Website',
+        image: 'music.png', // Public path to the image
+        description: 'Developed a Python web application that enables users to explore the top 50 most popular songs across any music genre. The project leverages Flask for the web framework and integrates with the Spotify API to fetch real-time music data. Key features include a user-friendly interface with a dropdown menu for popular genres, scheduled weekly data updates for the latest charts, and enhanced styling using CSS and JavaScript to provide an engaging user experience.',
+        github: 'https://github.com/AlexZorrilla99/Music-Website',
+        website: '',
       }, 
+      /*
       {
         title: 'Deadplant (Strategy Making Website)',
         image: '/assets/images/deadplant2.png', // Public path to the image
